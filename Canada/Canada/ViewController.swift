326727f7d13a503dd89a950d1368b57410b72f16
Manoj-Kumar-V <manojmano2594@gmail.com>
2020-04-25 14:49:11 +0530

    Api integration and basic tableview setup.

--- Canada/Canada/ViewController.swift
+++ Canada/Canada/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var rows: [row] = []
+    let cellIdentifier = "myCell"
+    var myTableView: UITableView  = UITableView()
+    var responseFromApi : CanadaResponse?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Make api call and assign the response.
+        apiHandler.shared.makeApiCall(onSuccess: { (response) in
+            self.responseFromApi = response
+            guard let rows = self.responseFromApi?.rows else {return}
+            self.rows = rows
+            DispatchQueue.main.async {
+                self.myTableView.reloadData()
+            }
+        }) { (error) in
+            print("Error")
+        }
+        self.navigationItem.title = self.responseFromApi?.title
+        myTableView.tableFooterView = UIView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Get main screen bounds
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        myTableView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        self.view.addSubview(myTableView)
+        
+    }
+    
+    // MARK: TableView DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+        cell.textLabel?.text = self.rows[indexPath.row].title
+        cell.detailTextLabel?.text = self.rows[indexPath.row].description
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 
-
+        let sectionName = self.responseFromApi?.title
+        return sectionName
+    }
+    
 }
-
---
+++ Canada/Canada/apiHandler.swift
@@ -0,0 +1,53 @@
+//
+//  apiHandler.swift
+//  Canada
+//
+//  Created by Manoj on 25/04/20.
+//  Copyright © 2020 Manoj. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CanadaResponse: Decodable {
+    let title: String?
+    let rows: [row]?
+}
+
+struct row: Decodable {
+    let title: String?
+    let description: String?
+    let imageHref: String?
+    
+}
+class apiHandler {
+    
+    static let shared = apiHandler()
+    
+    func makeApiCall(onSuccess: ((CanadaResponse) -> Void)? , onError: ((NSError) -> Void)? = nil ){
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts")!
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            // ensure there is no error for this HTTPS response
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            
+            // ensure there is data returned from this HTTPS response
+            guard let content = data else {
+                print(NSLocalizedString("No data", comment: "No data"))
+                return
+            }
+            let tempStr = String.init(data: content, encoding: String.Encoding.isoLatin1)
+            guard let encodedData = tempStr?.data(using: String.Encoding.utf8) else {return}
+            let canadaRes: CanadaResponse = try! JSONDecoder().decode(CanadaResponse.self, from: encodedData)
+            onSuccess!(canadaRes)
+        }
+        task.resume()
+        
+    }
+}
